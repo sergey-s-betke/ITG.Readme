c54a8d52a13fafffde10e3b539a5aa4c5f79db1f
Sergey S. Betke <sergey.s.betke@yandex.ru>
2012-11-09 11:52:59 +0400

    * 1.0.0 + ConvertFrom-Dictionary + ConvertTo-HashTable
    Signed-off-by: Sergey S. Betke <sergey.s.betke@yandex.ru>

---
+++ ITG.Utils.psm1
@@ -0,0 +1,93 @@
+﻿function ConvertFrom-Dictonary {
+	<#
+		.Synopsis
+		    Конвертация таблицы транслитерации (да и не только) в массив объектов с целью дальнейшей сериализации.
+		.Parameter InputObject
+		    Таблица транслитерации.
+		.Example
+		    @{
+		        'А'='A';
+		        'Б'='B';
+		        'В'='V';
+		        'Г'='G';
+			} `
+			| ConvertFrom-Dictonary `
+			;
+	#>
+	
+    
+    param (
+		# Исходный словарь для конвейеризации
+		[Parameter(
+			Mandatory=$true,
+			Position=0,
+			ValueFromPipeline=$true
+		)]
+		[AllowEmptyCollection()]
+		[System.Collections.IDictionary]
+		$InputObject = @{}
+	)
+
+	process {
+		foreach ($key in $InputObject.keys) {
+	        New-Object -TypeName PSObject `
+			| Select-Object -Property `
+				@{ Name='Key'; Expression={ $key } } `
+				, @{ Name='Value'; Expression={ $InputObject[$key] } }
+	        ;
+		};
+	}
+}  
+
+function ConvertTo-HashTable {
+	<#
+		.Synopsis
+		    Преобразование однотипных объектов со свойствами key и value в hashtable.
+		.Example
+		    @{
+		        'А'='A';
+		        'Б'='B';
+		        'В'='V';
+		        'Г'='G';
+			} `
+			| ConvertFrom-Dictonary `
+			| ? { 'А','Б' -contains $_.key } `
+			| ConvertTo-HashTable `
+			;
+	#>
+
+	param (
+		# Ключ key для hashtable.
+		[Parameter(
+			Mandatory=$true,
+			Position=0,
+			ValueFromPipelineByPropertyName=$true
+		)]
+		[ValidateNotNullOrEmpty()]
+		[string]
+		$Key
+	,
+		# Значение Value для hashtable.
+		[Parameter(
+			Mandatory=$true,
+			Position=1,
+			ValueFromPipelineByPropertyName=$true
+		)]
+		$Value
+	)
+
+	begin {
+		$output = @{};
+	}
+	process {
+		$output.Add( $key, $Value );
+	}
+	end {
+		return $output;
+	}
+}  
+
+Export-ModuleMember `
+    ConvertFrom-Dictonary `
+    , ConvertTo-HashTable `
+;
---
+++ test.ps1
@@ -0,0 +1,65 @@
+﻿Import-Module `
+    (join-path `
+        -path ( ( [System.IO.FileInfo] ( $myinvocation.mycommand.path ) ).directory ) `
+        -childPath 'ITG.Utils' `
+    ) `
+    -force `
+	-Verbose `
+;
+
+set-variable `
+    -name test `
+    -value `
+    @{
+        'А'='A';
+        'Б'='B';
+        'В'='V';
+        'Г'='G';
+        'Д'='D';
+        'Е'='E';
+        'Ё'='E';
+        'Ж'='ZH';
+        'З'='Z';
+        'И'='I';
+        'Й'='I';
+        'К'='K';
+        'Л'='L';
+        'М'='M';
+        'Н'='N';
+        'О'='O';
+        'П'='P';
+        'Р'='R';
+        'С'='S';
+        'Т'='T';
+        'У'='U';
+        'Ф'='F';
+        'Х'='KH';
+        'Ц'='TC';
+        'Ч'='CH';
+        'Ш'='SH';
+        'Щ'='SHCH';
+        'Ь'='';
+        'Ы'='Y';
+        'Ъ'='';
+        'Э'='E';
+        'Ю'='IU';
+        'Я'='IA';
+    } `
+;
+
+<#
+$test `
+| ConvertFrom-Dictonary `
+;
+#>
+
+@{
+    'А'='A';
+    'Б'='B';
+    'В'='V';
+    'Г'='G';
+} `
+| ConvertFrom-Dictonary `
+| ? { 'А','Б' -contains $_.key } `
+| ConvertTo-HashTable `
+;
