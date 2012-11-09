3be378c5a78dfdcc19a6644c956cc04e897f4f3d
Sergey S. Betke <sergey.s.betke@yandex.ru>
2012-11-09 21:37:31 +0400

    * 1.1.0 * переименованы командлеты - отлажена командлета Add-Pair для различных сценариев использования конвейера * to-do: Add-Pair нужно проверить на возможность использования без конвейера * to-do: Add-CustomMember недоработана. Только один вариант использования конвейера реализован. По возможности - заменить на обёртку для Add-Member
    Signed-off-by: Sergey S. Betke <sergey.s.betke@yandex.ru>

--- ITG.Utils.psm1
+++ ITG.Utils.psm1
@@ -1,27 +1,25 @@
-﻿function ConvertFrom-Dictonary {
+﻿function Get-Pair {
 	<#
 		.Synopsis
-		    Конвертация таблицы транслитерации (да и не только) в массив объектов с целью дальнейшей сериализации.
-		.Parameter InputObject
-		    Таблица транслитерации.
+			Конвертация таблицы транслитерации и любых других словарей в массив объектов с целью дальнейшей сериализации.
 		.Example
-		    @{
-		        'А'='A';
-		        'Б'='B';
-		        'В'='V';
-		        'Г'='G';
+			@{
+				'А'='A';
+				'Б'='B';
+				'В'='V';
+				'Г'='G';
 			} `
-			| ConvertFrom-Dictonary `
+			| Get-Pair `
 			;
 	#>
 	
-    
-    param (
+	
+	param (
 		# Исходный словарь для конвейеризации
 		[Parameter(
-			Mandatory=$true,
-			Position=0,
-			ValueFromPipeline=$true
+			Mandatory=$true
+			, Position=0
+			, ValueFromPipeline=$true
 		)]
 		[AllowEmptyCollection()]
 		[System.Collections.IDictionary]
@@ -29,39 +27,50 @@
 	)
 
 	process {
-		foreach ($key in $InputObject.keys) {
-	        New-Object -TypeName PSObject `
-			| Select-Object -Property `
-				@{ Name='Key'; Expression={ $key } } `
-				, @{ Name='Value'; Expression={ $InputObject[$key] } }
-	        ;
-		};
+		$InputObject.GetEnumerator();
 	}
-}  
+}
 
-function ConvertTo-HashTable {
+function Add-Pair {
 	<#
 		.Synopsis
-		    Преобразование однотипных объектов со свойствами key и value в hashtable.
+			Преобразование / добавление однотипных объектов со свойствами key и value в hashtable / любой другой словарь.
 		.Example
-		    @{
-		        'А'='A';
-		        'Б'='B';
-		        'В'='V';
-		        'Г'='G';
+			@{
+				'А'='A';
+				'Б'='B';
+				'В'='V';
+				'Г'='G';
 			} `
-			| ConvertFrom-Dictonary `
+			| Get-Pair `
 			| ? { 'А','Б' -contains $_.key } `
-			| ConvertTo-HashTable `
+			| Add-Pair -PassThru `
 			;
+		.Example
+			@{
+				'А'='A';
+				'Б'='B';
+				'В'='V';
+				'Г'='G';
+			} `
+			#| get-pair `
+			#| Add-Pair -InputObject (@{a=2;zzzzzzzzzzzz=3}) -PassThru
+		.Example
+			@{
+				'А'='A';
+				'Б'='B';
+				'В'='V';
+				'Г'='G';
+			} `
+			| Add-Pair -key zzzzzzzzzzzz -value 3 -PassThru
 	#>
 
 	param (
 		# Ключ key для hashtable.
 		[Parameter(
-			Mandatory=$true,
-			Position=0,
-			ValueFromPipelineByPropertyName=$true
+			Mandatory=$true
+			, Position=0
+			, ValueFromPipelineByPropertyName=$true
 		)]
 		[ValidateNotNullOrEmpty()]
 		[string]
@@ -69,25 +78,135 @@ function ConvertTo-HashTable {
 	,
 		# Значение Value для hashtable.
 		[Parameter(
-			Mandatory=$true,
-			Position=1,
-			ValueFromPipelineByPropertyName=$true
+			Mandatory=$true
+			, Position=1
+			, ValueFromPipelineByPropertyName=$true
 		)]
 		$Value
+	,
+		# Исходный словарь, в который будут добавлены сопоставления.
+		[Parameter(
+			Mandatory=$false
+			, ValueFromPipeline=$true
+		)]
+		[AllowEmptyCollection()]
+		[System.Collections.IDictionary]
+		$InputObject = @{}
+	,
+		[switch]
+		$PassThru
 	)
 
 	begin {
-		$output = @{};
+		if ( $InputObject ) {
+			$res = $InputObject;
+		} else {
+			$res = @{};
+		};
 	}
 	process {
-		$output.Add( $key, $Value );
+		if ( $_ -is [System.Collections.IDictionary] ) {
+			$_.Add( $Key, $Value );
+		} else {
+			$res.Add( $Key, $Value );
+		};
+		if ( $PassThru -and ( $_ -is [System.Collections.IDictionary] ) ) {
+			return $_;
+		};
 	}
 	end {
-		return $output;
+		if ( $PassThru ) { return $res; };
 	}
-}  
+}
+
+function Add-CustomMember {
+	<#
+		.Synopsis
+			Преобразование однотипных объектов со свойствами key и value в единый объект,
+			свойства которого определены поданными на конвейер парами.
+		.Example
+			@{
+				'А'='A';
+				'Б'='B';
+				'В'='V';
+				'Г'='G';
+			} `
+			| Add-CustomMember `
+			;
+	#>
+	
+	[CmdletBinding(
+	)]
+
+	param (
+		# Идентификатор свойства
+		[Parameter(
+			Mandatory=$true
+			, Position=0
+			, ValueFromPipelineByPropertyName=$true
+		)]
+		[ValidateNotNullOrEmpty()]
+		[string]
+		[Alias("Key")]
+		$Name
+	,
+		# Значение Value для hashtable
+		[Parameter(
+			Mandatory=$true
+			, Position=1
+			, ValueFromPipelineByPropertyName=$true
+		)]
+		$Value
+#	,
+#		# Тип добавляемого члена объекта
+#		[Parameter(
+#			Mandatory=$false
+#			, ValueFromPipelineByPropertyName=$true
+#		)]
+#		[System.Management.Automation.PSMemberTypes]
+#		$MemberType = [System.Management.Automation.PSMemberTypes]::NoteProperty
+#	,
+#		# Исходный словарь, в который будут добавлены сопоставления.
+#		[Parameter(
+#			Mandatory=$false
+#		)]
+#		[PSObject]
+#		$InputObject = ( New-Object -TypeName PSObject )
+#	,
+#		[switch]
+#		$PassThru
+	,
+		[switch]
+		$Force
+	)
+
+	begin {
+#		if ( $InputObject ) {
+#			$res = $InputObject;
+#		} else {
+			$res = New-Object -TypeName PSObject;
+#		};
+	}
+	process {
+#		if ( $res ) {
+			Add-Member `
+				-InputObject $res `
+				-MemberType NoteProperty `
+				@PSBoundParameters `
+			;
+#		} else {
+#			Add-Member -MemberType NoteProperty @PSBoundParameters;
+#		};
+	}
+	end {
+#		if ( $PassThru ) {
+			return $res;
+#		};
+	}
+}
 
 Export-ModuleMember `
-    ConvertFrom-Dictonary `
-    , ConvertTo-HashTable `
+	Get-Pair `
+	, Add-Pair `
+	, Add-CustomMember `
 ;
