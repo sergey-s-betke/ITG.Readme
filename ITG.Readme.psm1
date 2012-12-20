e1a37dea5c78484b13c038a0bb8e65b1c40f0433
Sergey S. Betke <sergey.s.betke@yandex.ru>
2012-12-20 22:49:00 +0400

    - `_` распознаётся как допустимый символ в url * вынес регулярные выражения в ITG.RegExps 1.1.0 * подключил собственно модуль ITG.RegExps

--- ITG.Readme.psm1
+++ ITG.Readme.psm1
@@ -1,29 +1,6 @@
-﻿[System.Text.RegularExpressions.Regex]$reDomain = `
-	"(?<domain>(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+(?:aero|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|[a-zA-Z]{2}))";
-[System.Text.RegularExpressions.Regex]$rePort = `
-	"(?:[:](?<port>\d+))";
-[System.Text.RegularExpressions.Regex]$reSocket = `
-	"(?<socket>${reDomain}${rePort}?)";
-[System.Text.RegularExpressions.Regex]$reSchema = `
-	"(?<schema>http|https|ftp)";
-[System.Text.RegularExpressions.Regex]$reURLToken = `
-	"(?:(?:%[0-9a-fA-F]{2})|[a-zA-Z0-9`-`.]|&[a-z]+;)";
-[System.Text.RegularExpressions.Regex]$reURLPathEl = `
-	"${reURLToken}+";
-[System.Text.RegularExpressions.Regex]$reURLPath = `
-	"(?<path>${reURLPathEl}(?:/${reURLPathEl})*)";
-[System.Text.RegularExpressions.Regex]$reURLParam = `
-	"(?<paramName>${reURLToken}+)=(?<paramValue>${reURLToken}+)";
-[System.Text.RegularExpressions.Regex]$reURLParams = `
-	"(?:(?:[?])(?<params>${reURLParam}(?:&${reURLParam})*))";
-[System.Text.RegularExpressions.Regex]$reURLAnchor = `
-	"(?:(?:#)(?<anchor>${reURLToken}+))";
-[System.Text.RegularExpressions.Regex]$reURL = `
-	"(?<url>${reSchema}://${reDomain}${rePort}?(?:/${reURLPath}?${reURLParams}?${reURLAnchor}?)?)";
-[System.Text.RegularExpressions.Regex]$reURLShortHTTP = `
-	"(?<url>(?<domain>(?:www)`.${reDomain})${rePort}?(?:/${reURLPath}?${reURLParams}?${reURLAnchor}?)?)";
-[System.Text.RegularExpressions.Regex]$reURLShortFTP = `
-	"(?<url>(?<domain>(?:ftp)`.${reDomain})${rePort}?(?:/${reURLPath}?${reURLParams}?${reURLAnchor}?)?)";
+﻿'ITG.RegExps' `
+| Import-Module `
+;
 
 $PowerShellBaseHelpUrl = 'http://go.microsoft.com/fwlink/?LinkID=';
 $PowerShellAboutTopics = @{
