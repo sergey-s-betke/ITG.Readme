c4ac462e94bfb407efdc35be3849fad6f0caaeaf
Sergey S. Betke <sergey.s.betke@yandex.ru>
2013-10-11 16:25:17 +0400

    + добавил в процесс сборки запуск тестов на базе pester framework (#130), но пока самих тестов нет.

--- build.psake.test.ps1
+++ build.psake.test.ps1
@@ -1,26 +1,13 @@
 $psake.use_exit_on_error = $true;
 
-properties {
-	$CurrentDir = Resolve-Path . ;
-	$ModuleName = Split-Path -Path '.' -Leaf -Resolve;
-	$Invocation = Get-Variable MyInvocation -Scope 1 -ValueOnly;
-}
-
 task default -depends Test;
 
 task Test {
-<#
-	$here = (Split-Path -parent $MyInvocation.MyCommand.Definition);
+	Import-Module `
+		-Name Pester `
+		-Force `
+		-ErrorAction Stop `
+	;
 
-	Import-Module ($here + "\PsGet\PsGet.psm1") -Force;
-
-	try {
-    	Import-Module Pester -Force
-	} catch {
-    	Write-Warning "Unable to import module 'Pester' required for testing, attempting to install Pester via PsGet module ... "
-    	Install-Module pester
-	};
-
-	Invoke-Pester -relative_path $here -EnableExit:$EnableExit
-#>
+	Invoke-Pester;
 };
